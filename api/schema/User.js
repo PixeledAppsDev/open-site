5abcb8aec72c8bb81aba53d69113f34ae23ebc21
kishan789 <49410778+kishan789@users.noreply.github.com>
2021-06-02 15:18:29 +0530

    added email sending

--- api/schema/User.js
+++ api/schema/User.js
@@ -13,7 +13,6 @@ const UserSchema = gql`
     email: String!
     username: String!
     password: String!
-    validationcode: String!
     resetToken: String
     resetTokenExpiry: String
     image: File
@@ -58,7 +57,7 @@ const UserSchema = gql`
     username: String!
     fullName: String!
     password: String!
-    validationcode: String!
+    
   }
 
   input RequestPasswordResetInput {
